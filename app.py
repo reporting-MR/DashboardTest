fe6bb9e9079567e0e1cd056902355318b8a66a74
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-10-20 15:47:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,10 @@ from google.cloud import bigquery
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
-
-
 client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
 
 query = '''SELECT * FROM `sunpower-375201.sunpower_agg.sunpower_full_funnel` WHERE Date = "2023-10-17" LIMIT 10'''
 data = pandas.read_gbq(query, credentials=credentials)
