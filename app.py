2955dce1fb8e56d2e6641e032dc4d7f368d2e222
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-10-20 17:26:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,26 +50,11 @@ col4.metric(label = "CTR", value = ctr)
 col5.metric(label = "CVR", value = cvr)
 col6.metric(label = "CPC", value = round(cpc,2))
 
-fig = make_subplots(rows=1, cols=2)
-
-fig.add_trace(
-    go.Pie(names=data['State_Name'], values=data['Cost'], title='Cost by State'),
-    row=1, col=1
-)
-
-fig.add_trace(
-    go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
-    row=1, col=2
-)
-
-fig.update_layout(data, x ='Cost', y='Conversions', trendline='ols', title='Conversions vs Cost')
-fig.show()
-
 # Pie chart showing Conversions by Campaign
-#fig_pie = px.pie(data, names='State_Name', values='Cost', title='Cost by State')
-#fig_pie.update_traces(textposition='inside')
-#st.plotly_chart(fig_pie, use_container_width=True)
+fig_pie = px.pie(data, names='State_Name', values='Cost', title='Cost by State')
+fig_pie.update_traces(textposition='inside')
+st.plotly_chart(fig_pie, use_container_width=True)
 
 # Scatter plot showing Conversions as a function of cost with a regression line
-#fig_scatter = px.scatter(data, x ='Cost', y='Conversions', trendline='ols', title='Conversions vs Cost')
-#st.plotly_chart(fig_scatter, use_container_width=True)
+fig_scatter = px.scatter(data, x ='Cost', y='Conversions', trendline='ols', title='Conversions vs Cost')
+st.plotly_chart(fig_scatter, use_container_width=True)
