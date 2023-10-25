7c66953398fe77f5c91708af5d0a01493a7fe13c
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2023-10-25 11:52:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ daily_impressions = data.groupby(data['Date'].dt.date)['Impressions'].sum().rese
 daily_data['Impressions'] = daily_impressions['Impressions']
 daily_data['CTR'] = daily_data['Clicks'] / daily_data['Impressions']
 
-numerical_columns = df.select_dtypes(include=['number']).columns
+numerical_columns = data.select_dtypes(include=['number']).columns
 daily_sums = data.groupby(data['Date'].dt.date)[numerical_columns].sum()
 st.write(daily_sums)
 
